91106652704646205f16432519e2840efb69aac3
shahanaz98 <shahanazkhan98@gmail.com>
2026-08-12 13:22:01 +0530

    Update navbar to moblie use

--- app.js
+++ app.js
@@ -131,7 +131,7 @@ app.use((err, req, res, next) => {
     // res.status(statusCode).send(message);
 });
 
-app.listen(8080, () => {
+app.listen(8080, "0.0.0.0", () => {
     console.log("Server is listening on port 8080");
 
 });
